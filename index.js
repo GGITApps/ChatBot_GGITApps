d968662eaedbcd0799ee85ff8d2152c3d64b27f1
Camilo Andres Salinas Martinez <camilo@ggitapps.com>
2018-05-23 06:12:02 -0500

    Cambios al callback

--- index.js
+++ index.js
@@ -35,67 +35,162 @@ app.get("/webhook", (req, res)=>{
 
 })
 
-// Callbacks
-app.post("/webhook", function (req, res) {
-  // Make sure this is a page subscription
-  if (req.body.object == "page") {
-    // Iterate over each entry
-    // There may be multiple entries if batched
-    req.body.entry.forEach(function(entry) {
-      // Iterate over each messaging event
-      entry.messaging.forEach(function(event) {
-        if (event.postback) {
-          processPostback(event);
-        }
-      });
-    });
+app.post('/webhook', function(req, res){
 
-    res.sendStatus(200);
-  }
+	var data = req.body;
+	if(data.object == 'page'){
+
+		data.entry.forEach(function(pageEntry){
+			pageEntry.messaging.forEach(function(messagingEvent){
+
+				if(messagingEvent.message){
+					receiveMessage(messagingEvent);
+				}
+
+			});
+		});
+		res.sendStatus(200);
+	}
 });
 
-function processPostback(event) {
-  var senderId = event.sender.id;
-  var payload = event.postback.payload;
 
-  if (payload === "Greeting") {
-    // Get user's first name from the User Profile API
-    // and include it in the greeting
-    request({
-      url: "https://graph.facebook.com/v2.6/" + senderId,
-      qs: {
-        access_token: process.env.PAGE_ACCESS_TOKEN,
-        fields: "first_name"
-      },
-      method: "GET"
-    }, function(error, response, body) {
-      var greeting = "";
-      if (error) {
-        console.log("Error getting user's name: " +  error);
-      } else {
-        var bodyObj = JSON.parse(body);
-        name = bodyObj.first_name;
-        greeting = "Hi " + name + ". ";
+function receiveMessage(event){
+	var senderID = event.sender.id;
+	var messageText = event.message.text;
+
+
+	evaluateMessage(senderID, messageText);
+}
+
+function evaluateMessage(recipientId ,message){
+	var finalMessage = '';
+
+	if(isContain(message, 'ayuda')){
+		finalMessage = 'Por el momento no te puedo ayudar';
+
+	}else if(isContain(message, 'gato')){
+
+	 sendMessageImage(recipientId);
+
+	}else if(isContain(message, 'clima')){
+
+		getWeather(function(temperature){
+
+			message = getMessageWeather(temperature);
+			sendMessageText(recipientId,message);
+
+		});
+
+	}else if(isContain(message, 'info')){
+
+	 sendMessageTemplate(recipientId);
+
+	}else{
+		finalMessage = 'solo se repetir las cosas : ' + message;
+	}
+	sendMessageText(recipientId,finalMessage);
+}
+
+function sendMessageText(recipientId, message){
+	var messageData = {
+		recipient : {
+			id : recipientId
+		},
+		message: {
+			text: message
+		}
+	};
+	callSendAPI(messageData);
+}
+
+function sendMessageImage(recipientId){
+	var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "image",
+        payload: {
+          url: "http://i.imgur.com/SOFXhd6.jpg"
+        }
       }
-      var message = greeting + "Estamos bajo pruebas en nuestro bot, visitanos mas tarde 😄";
-      sendMessage(senderId, {text: message});
-    });
+    }
+  };
+	callSendAPI(messageData);
+}
+
+
+function sendMessageTemplate(recipientId){
+	var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      attachment: {
+        type: "template",
+        payload: {
+          template_type: "generic",
+          elements: [ elemenTemplate() ]
+        }
+      }
+    }
+  };
+	callSendAPI(messageData);
+}
+
+function elemenTemplate(){
+	return {
+	  title: "Eduardo Ismael",
+	  subtitle: "Desarrollado de Software en Código facilito",
+	  item_url: "https://www.facebook.com/codigofacilito/?fref=ts",
+	  image_url: "http://i.imgur.com/SOFXhd6.jpg",
+	  buttons: [ buttonTemplate() ],
   }
 }
 
-// sends message to user
-function sendMessage(recipientId, message) {
-  request({
-    url: "https://graph.facebook.com/v2.6/me/messages",
-    qs: {access_token: process.env.PAGE_ACCESS_TOKEN},
-    method: "POST",
-    json: {
-      recipient: {id: recipientId},
-      message: message,
-    }
-  }, function(error, response, body) {
-    if (error) {
-      console.log("Error sending message: " + response.error);
-    }
-  });
+function buttonTemplate(){
+	return{
+		type: "web_url",
+		url : "https://www.facebook.com/codigofacilito/?fref=ts",
+		title : "Codigo Facilito"
+	}
+}
+
+function callSendAPI(messageData){
+	request({
+		uri: 'https://graph.facebook.com/v2.6/me/messages',
+		qs : { access_token :  APP_TOKEN },
+		method: 'POST',
+		json: messageData
+	}, function(error, response, data){
+
+		if(error){
+			console.log('No es posible enviar el mensaje');
+		}else{
+			console.log("El mensaje fue enviado");
+		}
+
+	});
+}
+
+function getMessageWeather(temperature){
+	if (temperature > 30)
+		return "Nos encontramos a "+ temperature +" Hay demaciado calor, te recomiendo que no salgas";
+	return "Nos encontramos a "+ temperature +" es un bonito día para salir";
+}
+
+function getWeather(  callback ){
+	request('http://api.geonames.org/findNearByWeatherJSON?lat=16.750000&lng=-93.116669&username=demo',
+		function(error, response, data){
+			if(!error){
+				var response = JSON.parse(data);
+				var temperature = response.weatherObservation.temperature;
+				callback(temperature);
+			}
+		});
+}
+
+function isContain(sentence, word){
+	return sentence.indexOf(word) > -1;
 }
